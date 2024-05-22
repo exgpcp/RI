fa6454d8b6c30bd266e9bcdb357397d0529d228c
exgpcp <bt512061@gmail.com>
2024-05-22 00:26:42 -0700

    Update Oracle MLE for hyperparamters.R

--- synthetic_examples/synthetic-example-1/Oracle MLE for hyperparamters.R
+++ synthetic_examples/synthetic-example-1/Oracle MLE for hyperparamters.R
@@ -18,6 +18,7 @@ points_inhomo=get(nam)
 N=length(points_inhomo)
 measure_sup1=3 #supreme
 T1=50
+cc=1 #cc=2 corresponds to the second 100 datasets in the first synthetic example
 
 expo_quad_kernel<-function(theta00,theta11,xn,xm){ # 1,0.1
   return(theta00*exp(-theta11/2*sum((xn - xm)**2)))
@@ -65,6 +66,5 @@ groundest<-function(c,func,T,u1,u2,noise_var){
   return(out$optim$bestmem)
 }
 
-cc=1 #cc=2 corresponds to the second 100 datasets in the first synthetic example
 a=groundest(cc,inten1,T1,20,1,1e-5)
 save(a, file =paste0( "/groundest/func1_s1_groundest",args[1],".rda"))
