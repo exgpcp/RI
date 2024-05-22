d5964b2726d0211a3c48c4cb188e60841cdbee33
exgpcp <bt512061@gmail.com>
2024-05-21 20:30:31 -0700

    Update weighted MAP for hyperparameters.R

--- synthetic_examples/synthetic-example-1/weighted MAP for hyperparameters.R
+++ synthetic_examples/synthetic-example-1/weighted MAP for hyperparameters.R
@@ -34,14 +34,6 @@ inten2<-function(x){
   return(10+x-x)
 }
 
-inten3<-function(x){
-  if(x<=25){return(2+1/25*x)}
-  if(x<=50){return(5-2/25*x)}
-  if(x<=75){return(3/50*x-2)}
-  return(1/50*x+1)
-  
-}
-
 #measure_sup1=3 #supreme
 T1=50
 #bin_num1=1000
@@ -54,14 +46,7 @@ T2=5
 #x2=seq(0,T2,length.out=bin_num2)
 #intensity2=inten2(x2)
 
-#measure_sup3=3.1 #supreme
-T3=100
-#bin_num3=1000
-#x3=seq(0,T3,length.out=bin_num3)
-#intensity3=sapply(x3,inten3)
-
-
-nam <- paste0("data_",args[1])
+nam <- paste0("syndata/data_",args[1])
 points_inhomo=get(nam)
 noise_var=1e-5
 ccc=4
